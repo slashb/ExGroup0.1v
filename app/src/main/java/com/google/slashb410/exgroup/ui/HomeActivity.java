5b9d28d862059040e6a7bf4232e4c25a16722985
Tacademy <slashb410@gmail.com>
2017-01-31 17:22:01 +0900

    test4 commit

--- app/src/main/java/com/google/slashb410/exgroup/ui/HomeActivity.java
+++ app/src/main/java/com/google/slashb410/exgroup/ui/HomeActivity.java
@@ -2,9 +2,14 @@ package com.google.slashb410.exgroup.ui;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.google.slashb410.exgroup.R;
 import com.google.slashb410.exgroup.ui.mypage.MyHomeActivity;
+import com.google.slashb410.exgroup.ui.settings.AccountSettingActivity;
+import com.google.slashb410.exgroup.ui.settings.PushSettingActivity;
+import com.google.slashb410.exgroup.ui.settings.ToDeveloperActivity;
 import com.google.slashb410.exgroup.util.U;
 
 import butterknife.ButterKnife;
@@ -17,6 +22,39 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         ButterKnife.bind(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_home, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+
+        switch(id) {
+            case R.id.action_push:
+                U.getInstance().goNext(this, PushSettingActivity.class, false);
+                return true;
+            case R.id.action_account:
+                U.getInstance().goNext(this, AccountSettingActivity.class, false);
+                return true;
+            case R.id.action_to_developers:
+                U.getInstance().goNext(this, ToDeveloperActivity.class, false);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+
     }
 
     @OnClick(R.id.profile_box)
