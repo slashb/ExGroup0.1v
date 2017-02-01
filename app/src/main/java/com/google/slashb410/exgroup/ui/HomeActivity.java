ec03ac98358d3f9d2c191e0b5b9747c9c55eea8a
Tacademy <slashb410@gmail.com>
2017-02-01 14:02:23 +0900

    0201 commit

--- app/src/main/java/com/google/slashb410/exgroup/ui/HomeActivity.java
+++ app/src/main/java/com/google/slashb410/exgroup/ui/HomeActivity.java
@@ -1,11 +1,20 @@
 package com.google.slashb410.exgroup.ui;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.TextView;
 
 import com.google.slashb410.exgroup.R;
+import com.google.slashb410.exgroup.ui.group.create.GroupAddActivity;
 import com.google.slashb410.exgroup.ui.mypage.MyHomeActivity;
 import com.google.slashb410.exgroup.ui.settings.AccountSettingActivity;
 import com.google.slashb410.exgroup.ui.settings.PushSettingActivity;
@@ -17,11 +26,30 @@ import butterknife.OnClick;
 
 public class HomeActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         ButterKnife.bind(this);
+        final String[] groupName = {"슬비네그룹", "소담이네그룹", "혜원이네", "승옥이네", "추가"};
+
+
+        GridAdapter gridAdapter = new GridAdapter(this, R.layout.group_card_view, groupName);
+        GridView gridView = (GridView) findViewById(R.id.group_grid);
+        gridView.setAdapter(gridAdapter);
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                if(position==groupName.length-1){
+                    U.getInstance().goNext(getApplicationContext(), GroupAddActivity.class, false);
+                }
+            }
+        });
+
+
     }
 
     @Override
@@ -62,14 +90,51 @@ public class HomeActivity extends AppCompatActivity {
         U.getInstance().goNext(this, MyHomeActivity.class, false);
     }
 
-    @OnClick(R.id.tmp_group_in)
-    public void goGroup(){
 
+
+}
+
+class GridAdapter extends BaseAdapter{
+
+    Context context;
+    String[] groupName;
+    int layout;
+    LayoutInflater inflater;
+
+    public GridAdapter(Context context, int layout, String[] groupName) {
+        this.context = context;
+        this.layout = layout;
+        this.groupName = groupName;
+        inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     }
 
-    @OnClick(R.id.tmp_add_group)
-    public void addGroup(){
-        
+    @Override
+    public int getCount() {
+        return (groupName.length);
     }
 
+    @Override
+    public Object getItem(int position) {
+
+        return groupName[position];
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView==null)
+            convertView = inflater.inflate(layout, null);
+
+        TextView textView = (TextView) convertView.findViewById(R.id.group_name);
+            textView.setText(groupName[position]);
+
+
+
+        return convertView;
+
+    }
 }
--- app/src/main/java/com/google/slashb410/exgroup/ui/group/create/GroupAddActivity.java
+++ app/src/main/java/com/google/slashb410/exgroup/ui/group/create/GroupAddActivity.java
@@ -4,6 +4,10 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.google.slashb410.exgroup.R;
+import com.google.slashb410.exgroup.util.U;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class GroupAddActivity extends AppCompatActivity {
 
@@ -11,5 +15,12 @@ public class GroupAddActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_group_add);
+        ButterKnife.bind(this);
     }
+
+    @OnClick(R.id.group_add)
+    public void goAdd(){
+        U.getInstance().goNext(this, WeightCheckActivity.class, false);
+    }
+
 }
