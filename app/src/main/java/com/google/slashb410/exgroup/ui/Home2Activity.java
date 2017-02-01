32481e810cfc60fb534e25cf78d6a0a1cdfae87d
Tacademy <slashb410@gmail.com>
2017-02-01 17:33:36 +0900

    0201final commit

--- app/src/main/java/com/google/slashb410/exgroup/ui/Home2Activity.java
+++ app/src/main/java/com/google/slashb410/exgroup/ui/Home2Activity.java
@@ -1,9 +1,7 @@
 package com.google.slashb410.exgroup.ui;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -52,14 +50,14 @@ public class Home2Activity extends AppCompatActivity
         });
         //-------------------------------------------------------------------------------------------
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
--- app/src/main/java/com/google/slashb410/exgroup/ui/group/create/GroupAddActivity.java
+++ app/src/main/java/com/google/slashb410/exgroup/ui/group/create/GroupAddActivity.java
@@ -1,15 +1,25 @@
 package com.google.slashb410.exgroup.ui.group.create;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.DatePicker;
+import android.widget.Toast;
 
 import com.google.slashb410.exgroup.R;
 import com.google.slashb410.exgroup.util.U;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class GroupAddActivity extends AppCompatActivity {
+
+    final int DIALOG_DATE = 1;
+    String[] yyMMdd = whenToday();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,9 +28,59 @@ public class GroupAddActivity extends AppCompatActivity {
         ButterKnife.bind(this);
     }
 
+    @OnClick(R.id.datepickBtn)
+    public void pickDate(){
+        showDialog(DIALOG_DATE);
+    }
+
+    @Override
+    @Deprecated
+    protected Dialog onCreateDialog(int id) {
+        switch(id){
+            case DIALOG_DATE :
+                DatePickerDialog dpd = new DatePickerDialog
+                        (this, // 현재화면의 제어권자
+                                new DatePickerDialog.OnDateSetListener() {
+                                    public void onDateSet(DatePicker view,
+                                                          int year, int monthOfYear,int dayOfMonth) {
+                                        Toast.makeText(getApplicationContext(),
+                                                year+"년 "+(monthOfYear+1)+"월 "+dayOfMonth+"일 을 선택했습니다",
+                                                Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                                , // 사용자가 날짜설정 후 다이얼로그 빠져나올때
+                                //    호출할 리스너 등록
+                                Integer.parseInt(yyMMdd[0]), Integer.parseInt(yyMMdd[1]), Integer.parseInt(yyMMdd[2])); // 기본값 연월일
+                return dpd;
+
+        }
+
+
+        return super.onCreateDialog(id);
+    }
+
+    public String[] whenToday(){
+
+        String[] today = {"",};
+        long now = System.currentTimeMillis();
+
+        Date date = new Date(now);
+
+        SimpleDateFormat CurYearFormat = new SimpleDateFormat("yyyy");
+       SimpleDateFormat CurMonthFormat = new SimpleDateFormat("MM");
+        SimpleDateFormat CurDayFormat = new SimpleDateFormat("dd");
+
+        today[0] = CurYearFormat.format(date);
+        today[1] = CurMonthFormat.format(date);
+        today[2] = CurDayFormat.format(date);
+
+        return today;
+    }
+
     @OnClick(R.id.group_add)
     public void goAdd(){
         U.getInstance().goNext(this, WeightCheckActivity.class, false);
     }
+
 
 }
